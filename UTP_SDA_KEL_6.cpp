feac81704ab337a800f0f7ac6ee54168ce289b72
mourenemillie <lifeasnanaw@gmail.com>
2025-06-09 15:58:40 +0700

    Register user

--- UTP_SDA_KEL_6.cpp
+++ UTP_SDA_KEL_6.cpp
@@ -11,9 +11,23 @@ using namespace std;
 
 void registerUser (){
     string username, password ;
+    cout << "Masukkan username: ";
+    cin >> username;
+
+    if (isUsernameExist(username)){
+        cout << "Username sudah ada. Silahkan coba username lain." << endl;
+        return;
+    }
+
+    cout << "Masukkan password: ";
+    cin >> password;
+    ofstream file ("userdata.txt", ios::app);
+    file << username << " " << password << endl ;
+    cout << "Registrasi berhasil!" << endl;
+    
 }
 
 int main(){
-    
+
     return 0;
 }
